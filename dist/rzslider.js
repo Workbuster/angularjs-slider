8828015233944985002c545d1ff012d6be844862
jskogelin <jonathan@workbuster.com>
2016-02-15 15:52:33 +0100

    do not load angular module

--- dist/rzslider.js
+++ dist/rzslider.js
@@ -7,19 +7,7 @@
 (function(root, factory) {
   'use strict';
   /* istanbul ignore next */
-  if (typeof define === 'function' && define.amd) {
-    // AMD. Register as an anonymous module.
-    define(['angular'], factory);
-  } else if (typeof module === 'object' && module.exports) {
-    // Node. Does not work with strict CommonJS, but
-    // only CommonJS-like environments that support module.exports,
-    // like Node.
-    // to support bundler like browserify
-    module.exports = factory(require('angular'));
-  } else {
-    // Browser globals (root is window)
-    factory(root.angular);
-  }
+  factory(root.angular);
 
 }(this, function(angular) {
   'use strict';
